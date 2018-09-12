95b44d03353836f882eea1fa17717364e3a8506e
HealyDustin2019 <42677953+HealyDustin2019@users.noreply.github.com>
2018-09-12 09:12:41 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,5 @@
-public class PoeticProg { public static void main(String[] args){ System.out.print("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness"); }}
+public class PoeticProg {
+  public static void main(String[] args){ 
+    System.out.print("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
+  }
+}
